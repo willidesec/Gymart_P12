798210c7174fafe89784690ab74ad3081ebe1578
William Désécot <william.desecot@hotmail.fr>
2019-05-30 21:35:34 +0200

    Initialize Xcode project

---
+++ Gymart/Gymart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gymart
+//
+//  Created by William on 30/05/2019.
+//  Copyright © 2019 William Désécot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
