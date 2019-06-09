902397a1bbe7ab30288cdd291c9e49a62747f5c0
William Désécot <william.desecot@hotmail.fr>
2019-06-09 19:03:39 +0200

    [feat] add register screen

---
+++ Gymart/Gymart/Controller/Login&Register/RegisterViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RegisterViewController.swift
+//  Gymart
+//
+//  Created by William on 09/06/2019.
+//  Copyright © 2019 William Désécot. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    // MARK: - IBOutlet
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    // MARK: - IBAction
+    
+    
+    
+    // MARK: - Methods
+
+}
