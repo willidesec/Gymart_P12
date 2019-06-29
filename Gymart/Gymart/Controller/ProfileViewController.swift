dc38b10b8bc09b006eba4dd29765c68fd9f791bc
William Désécot <william.desecot@hotmail.fr>
2019-06-29 18:22:53 +0200

    [feat] add ProfileViewController

---
+++ Gymart/Gymart/Controller/ProfileViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileViewController.swift
+//  Gymart
+//
+//  Created by William on 29/06/2019.
+//  Copyright © 2019 William Désécot. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+    // MARK: - IBOutlet
+    
+    
+    // MARK: - View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    // MARK: - IBAction
+    @IBAction func signOutItemDidTapped(_ sender: UIBarButtonItem) {
+        print("sign out button dit tapped")
+    }
+    
+    
+}
