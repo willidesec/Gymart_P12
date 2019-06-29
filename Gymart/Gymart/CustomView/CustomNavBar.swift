7c9a2cc0fb76563ec94560b355e581f41ca400b5
William Désécot <william.desecot@hotmail.fr>
2019-06-29 18:17:47 +0200

    [feat] add Tab bar, profile, refactor storyboard

---
+++ Gymart/Gymart/CustomView/CustomNavBar.swift
@@ -0,0 +1,15 @@
+//
+//  CustomNavBar.swift
+//  Gymart
+//
+//  Created by William on 29/06/2019.
+//  Copyright © 2019 William Désécot. All rights reserved.
+//
+
+import UIKit
+
+class CustomNavBar: UINavigationBar {
+    override func awakeFromNib() {
+        self.barTintColor = UIColor.blue
+    }
+}
