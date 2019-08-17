999d5897cb8944c1c046027c1e313ebe85d4cb5c
William Désécot <william.desecot@hotmail.fr>
2019-08-17 17:30:05 +0200

    [feat]: add historical screen

---
+++ Gymart/Gymart/Controller/HistoricalViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HistoricalViewController.swift
+//  Gymart
+//
+//  Created by William on 17/08/2019.
+//  Copyright © 2019 William Désécot. All rights reserved.
+//
+
+import UIKit
+
+class HistoricalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
