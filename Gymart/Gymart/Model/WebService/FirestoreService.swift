d0335695517dd63ffe9d5806c7e9ba725f2db4da
William Désécot <william.desecot@hotmail.fr>
2019-09-08 16:49:10 +0200

    [feat]: create endpoint class

--- Gymart/Gymart/Model/WebService/FirestoreService.swift
+++ Gymart/Gymart/Model/WebService/FirestoreService.swift
@@ -52,38 +52,3 @@ final class FirestoreService {
         document?.updateData(data, completion: completion)
     }
 }
-
-enum Endpoint {
-    case user
-    case currentUser
-    case program
-    case workout(programId: String)
-    case training(programId: String, workoutId: String)
-    case historical
-}
-
-extension Endpoint {
-    var userId: String {
-        guard let currentUser = AuthService.getCurrentUser() else {
-            return "unknow user"
-        }
-        return currentUser.uid
-    }
-    
-    var path: String {
-        switch self {
-        case .user:
-            return "users"
-        case .currentUser:
-            return "users/\(userId)"
-        case .program:
-            return "users/\(userId)/programs"
-        case let .workout(programId):
-            return "users/\(userId)/programs/\(programId)/workouts"
-        case let .training(programId, workoutId):
-            return "users/\(userId)/programs/\(programId)/workouts/\(workoutId)"
-        case .historical:
-            return "users/\(userId)/historical"
-        }
-    }
-}
